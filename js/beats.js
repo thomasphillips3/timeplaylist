e3991e2eefe6b123ed210e27e9fb4507c4746670
Thomas Phillips <tpphilli@amazon.com>
2016-03-21 23:39:07 -0700

    Enable SoundCloud auth
    * Cleaned up html files
    * Removed (commented out) embedded player
    * Moved js code from HTML to js file
    * Enabled SoundCloud auth

--- js/beats.js
+++ js/beats.js
@@ -1,3 +1,14 @@
-$(document).ready(function(){
+(function(){
   console.log("this iife tho");
-})
+  SC.initialize({
+    client_id: '1ccc81f4a0cc76fd11de2a4ee45be460',
+    redirect_uri: 'http://thomasphillips3.github.io/timeplaylist/callback.html'
+  });
+
+  // initiate auth popup
+  SC.connect().then(function() {
+    return SC.get('/me');
+  }).then(function(me) {
+    alert('Hello, ' + me.username);
+  });
+}())
