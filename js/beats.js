fda390ada4611a383b7222eb3cf6e3291bdff17e
Thomas Phillips <tpphilli@amazon.com>
2016-04-05 18:45:30 -0700

    Use https instead of http for redirect_uri

--- js/beats.js
+++ js/beats.js
@@ -2,7 +2,7 @@
   console.log("this iife tho");
   SC.initialize({
     client_id: '1ccc81f4a0cc76fd11de2a4ee45be460',
-    redirect_uri: 'http://thomasphillips3.github.io/timeplaylist/callback.html'
+    redirect_uri: 'https://thomasphillips3.github.io/timeplaylist/callback.html'
   });
 
   // initiate auth popup
