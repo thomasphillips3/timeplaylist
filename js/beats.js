14d1083b30bd2f316bbf96eabd856bb204a8bdf6
Thomas Phillips <tpphilli@amazon.com>
2016-04-06 16:05:58 -0700

    Removed some debug code. Started working on building tracklist.

--- js/beats.js
+++ js/beats.js
@@ -7,23 +7,10 @@
 
   // initiate auth popup
   SC.connect().then(function() {
-    console.log("connected");
     return SC.get('/me');
   }).then(function(me) {
-    // alert('Hello, ' + me.username);
-    // window.setTimeout(window.opener.SC.connectCallback, 1);
-    console.log("in the 'then' function");
   });
 
-  // var userId = 5333180;
-  // SC.get("/tracks", {
-  //   user_id: userId,
-  //   limit: 100
-  // }, function(tracks) {
-  //   var tmp = '';
-  //   for (var i = 0; i < tracks.length; i++) {
-  //     tmp = '<a href="' + tracks[i].permalink_url + '">' + tracks[i].title + ' - ' + tracks[i].duration + '</a>';
-  //     $("<li/>").html(tmp).appendTo("#track-list");
-  //   }
-  // });
+  var userId = 5333180;
+  tracks = SC.get("/playlists", {user_id: 5333180})._result;
 }())
